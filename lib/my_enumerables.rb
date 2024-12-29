4cc7d5e3971058a6e815ecbab7f426425e17bf60
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2024-12-29 18:36:02 +0100

    add impls

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,66 @@
+# For practice purposes, I am not just going to delegate to existing methods,
+# instead will implement each of them with #each. I am sure some of these has quirks I am ignoring
+# but if there were important enough, tests would have covered them :D
+
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    i = 0
+    each do |el|
+      yield(el, i)
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    result = []
+    each do |el|
+      include = yield(el)
+      result << el if include
+    end
+    result
+  end
+
+  def my_all?
+    each do |el, i|
+      return false unless yield(el, i)
+    end
+    true
+  end
+
+  def my_any?
+    each do |el, i|
+      return true if yield(el, i)
+    end
+    false
+  end
+
+  def my_none?(&block)
+    !my_any?(&block)
+  end
+
+  def my_count
+    count = 0
+    each do |el|
+      count += 1 if !block_given? || yield(el)
+    end
+    count
+  end
+
+  def my_map
+    results = []
+    each do |el|
+      results << yield(el)
+    end
+    results
+  end
+
+  def my_inject(acc = 0)
+    each do |el|
+      acc = yield(acc, el)
+    end
+    acc
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +68,12 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    i = 0
+    while i < size
+      yield self[i]
+      i += 1
+    end
+    self
+  end
 end
